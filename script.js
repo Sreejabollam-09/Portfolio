dba91c96fbbeb91a065d13088f55e082343463b8
Sreejabollam-09 <bollamsreeja09@gmail.com>
2026-03-31 15:28:34 +0530

    Update strings in Typed.js initialization

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* main.js */
 var typed = new Typed(".text", {
-    strings: ["Programming", "Cybersecurity", "Web Development"],
+    strings: ["Programming", "Web Designing", "Web Development"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
